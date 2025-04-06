8ff07e53cbf5e9ce5d305abfff8a77dca94fa3f3
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-04-06 06:09:31 +0500

    Export additional modules for improved functionality in ImageFlow, including lazy cache image, image configuration, utility functions, and cache provider.

--- lib/imageflow.dart
+++ lib/imageflow.dart
@@ -1,5 +1,10 @@
 library imageflow;
 
+export 'src/widgets/lazy_cache_image.dart';
+export 'src/models/image_config.dart';
+export 'src/utils/image_utils.dart';
+export 'src/providers/cache_provider.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
